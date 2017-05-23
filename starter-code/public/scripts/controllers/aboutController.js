953383472e6f71aa3435634c6eb702d648b7f5af
Stephanie Dover <doverstephaniem@gmail.com>
2017-05-23 13:29:43 -0700

    about and repoview

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -5,11 +5,12 @@ var app = app || {};
   const aboutController = {};
 
   aboutController.index = () => {
+
     $('#about').show().siblings().hide(); // REVIEW: We have a slight refactor in selectors here, which has reduced the amount of code from the last lab.
 
-    // TODO: Call a function to load all the data.
+    // DONE maybe: Call a function to load all the data.
     // Pass a view function as a callback, so the view will render after the data is loaded.
-
+    app.Article.fetchAll(app.repoView.index);
   };
 
   module.aboutController = aboutController;
